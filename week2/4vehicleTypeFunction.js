d8377ea64e6bc39ee12a83a273b1c4db79c6733c
Alaa Masoud <30554608+Alaamasoud@users.noreply.github.com>
2017-09-12 14:21:23 +0200

    Create 4vehicleTypeFunction.js

---
+++ week2/4vehicleTypeFunction.js
@@ -0,0 +1,11 @@
+let a = " blue "
+let b = " motorbike "
+
+function vehicleType(a, b){
+  console.log("a" + a + b);
+  return
+}
+
+vehicleType(a, b);
+vehicleType (" green ", " opel " )
+vehicleType ( " Yellow ", " Ferrari ")
