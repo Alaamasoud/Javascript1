ab903c0532be49a776a412bf1f0da72d8bd400aa
Alaa Masoud <30554608+Alaamasoud@users.noreply.github.com>
2017-09-12 14:17:10 +0200

    Create assPart2.js

---
+++ Week1/assPart2.js
@@ -0,0 +1,10 @@
+//Can you store multiple types in an array? Numbers and strings?
+
+let arr = ["string", 66, "string2", 33]
+
+console.log(typeof arr[0]); // string
+console.log(typeof arr[1]); // number
+
+//Can you compare inifities? (Not in Eyad's world) - does 6/0 == 10/0? How can you test this?
+
+console.log(10/0 == 6/0); // true
