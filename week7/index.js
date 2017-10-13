2bee77262b62abf7459200a9cdc8c08826f81922
Alaa Masoud <30554608+Alaamasoud@users.noreply.github.com>
2017-10-13 21:11:18 +0200

    Add hw week7

---
+++ week7/index.js
@@ -0,0 +1,154 @@
+let allRepos = []
+let repoNames = []
+let watcherCount = [];
+
+window.addEventListener('load', getRepos)
+
+function getRepos() {
+    let xhr = new XMLHttpRequest(); // creat a request    
+    xhr.open("GET", "https://api.github.com/orgs/HackYourFuture/repos"); // GET the request
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) { //check if status = 200 && statusText = OK
+            allRepos = JSON.parse(xhr.response)
+            for (let i of allRepos) {
+                let repoName = i.name.toLowerCase()
+                repoNames.push(repoName)
+            }
+            
+            
+        }
+    }
+    xhr.send(null);
+   
+}
+//console.log(repoNames);
+
+
+////////////////////////////////////////////
+////////////////////////////////////////////
+////////////////////////////////////////////
+
+
+
+
+function displayRepos() {
+    let reposDiv = document.getElementById("reposContainer");
+    reposDiv.innerHTML = "";
+    reposDiv.style.display = "block";
+
+    let reposList = document.createElement("ol");
+    reposDiv.appendChild(reposList);
+
+    for (let i of allRepos) { //convert the dataObject to an Array
+        let li = document.createElement("li");
+        li.innerHTML = '<a href="' + i.html_url + '"target="_blank">' + i.name + '</a>';
+        reposList.appendChild(li);
+        let repoName = i.name.toLowerCase()
+        repoNames.push(repoName)
+    }
+    
+    ////////////////////////////////////////////
+////////////////////////////////////////////
+////////////////////////////////////////////
+            
+           
+            for (let w of allRepos){
+                watcherCount.push(w.watchers);
+            }
+    
+            let Watchers = watcherCount.reduce((x, y) =>x + y);
+                let h3 = document.createElement("h3");
+                h3.innerHTML = "The total number of watchers for these repos is " + Watchers;
+                reposList.appendChild(h3);
+                console.log(Watchers);
+//                console.log(watcherCount); 
+}
+
+function checkValue(value) {
+    let bool = false
+    value = value.toLowerCase()
+    for (let i = 0; i < repoNames.length; i++) {
+        if (repoNames[i] === value) {
+            bool = true
+        }
+    }
+    return bool
+}
+
+function getApiByInput() {
+    let searchField = document.getElementById('search');
+    let searchValue = searchField.value;
+    let url = "https://github.com/HackYourFuture/" + searchValue;
+    if (checkValue(searchValue)) {
+        window.open(url);
+    } else {
+        alert('The value ' + searchValue + ' is not a repo name.')
+    }
+}
+
+
+
+let allUsers = []
+let userNames = []
+
+
+function getContrib() {
+    let searchField = document.getElementById('search');
+    let searchValue = searchField.value;
+    
+    let xhr = new XMLHttpRequest(); // creat a request    
+    let conribUrl = "https://api.github.com/repos/HackYourFuture/" + searchValue +"/contributors";
+    xhr.open("GET", conribUrl); // GET the request
+    
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) { //check if status = 200 && statusText = OK
+            allUsers = JSON.parse(xhr.response)
+            
+            
+                let userDiv = document.getElementById("contrContainer");
+                userDiv.innerHTML = "";
+                userDiv.style.display = "block";
+
+                let usersList = document.createElement("ul");
+                userDiv.appendChild(usersList);
+                
+                let userLi = document.createElement("li");
+                usersList.appendChild(userLi);
+                
+                let avatarImg = document.createElement('img');
+                 userDiv.appendChild(avatarImg);
+            
+            
+            for (let i in allUsers) {
+               console.log(allUsers[i].login);                         
+                     userLi.innerHTML =  userLi.innerHTML + allUsers[i].login+ '<br>';
+                
+                    avatarImg.setAttribute('src', allUsers[i].avatar_url)
+                    avatarImg.innerHTML = avatarImg.innerHTML + allUsers[i].avatar_url + '<br>';
+            }
+        }
+    }
+    xhr.send(null);
+//    console.log(xhr.status);
+//    console.log(xhr.statusText); 
+//    window.open(conribUrl);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
---
+++ week7/undestanding mapFilterReduce/test.js
@@ -0,0 +1,71 @@
+let numbers = [1,2,3,4,5,6,7,8,9,10];
+//console.log(numbers);
+
+//MAP
+
+//Arrow Function Syntax
+
+//let double = numbers.map(x => x * 2);            
+//
+//
+////Normal Function
+//let dobule2 = numbers.map(function(x){
+//    return x = x * 2;
+//})
+//
+//console.log(double,dobule2);
+//
+//
+////FILTER is Even
+////Arrow Function Syntax
+//let isEven = numbers.filter(x => x % 2 == 0); 
+//console.log(isEven);
+////
+////
+//////Normal Function
+//let isEvenLoop = numbers.filter(function(x){
+//    return x = x % 2 == 0 ;
+//});
+//
+//console.log(isEvenLoop);
+//                             
+////FILTER is Odd
+////Arrow Function Syntax
+//let isOdd = numbers.filter(x => x % 2);
+//console.log(isOdd);
+//
+//////Normal Function
+//let isOddLoop = numbers.filter(function(x){
+//    return x = x % 2;
+//});
+//console.log(isOddLoop);
+////
+
+
+
+
+ x = Math.ceil(10.126);
+console.log(x);
+
+   
+    
+
+
+
+//let names =[ {
+//    a:"Alaa",
+//    b:"AlaaJarida",
+//    c:"Tim"
+//},
+//{
+//     a:"Walaa",
+//    b:"Omar",
+//    c:"Toteh"
+//}           ]
+//
+////console.log(names[0]);
+//
+//for ( i of names){
+//    console.log(i);
+//}
+//
